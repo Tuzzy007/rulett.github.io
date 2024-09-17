c62a962d576b9123c1f56d01540b0a433a6efcb0
Tuzzy007 <121279943+Tuzzy007@users.noreply.github.com>
2024-09-17 17:56:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let tet = 0;
 let fogadasok = [];
 let nyeroSzam = -1;
 let nyeremeny = 0;
+let szamolaskezdes = false;
 
 const valasztas = {
     "40": [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36],
@@ -37,15 +38,19 @@ function selectCell(szam) {
         };
         
         if (szam >= 40) {
-            alert("Ezt választottad: " + valsztasok[szam] + ". A Téted: " + tet);
+            let tete = document.getElementById('tete');
+            tete.innerHTML += `Ezt választottad: ${valsztasok[szam]}, a Téted: ${tet}<br>`;
         } else {
-            alert("Ezt választottad: " + szam + ". A Téted: " + tet);
+            let tete = document.getElementById('tete');
+            tete.innerHTML += `Ezt választottad: ${szam}, a Téted: ${tet}<br>`;
         }
+        
 
         valasztott = szam;
         fogadasok.push({ szam: valasztott, tet: tet});
 
         console.log("Fogadások:", fogadasok);
+        szamolaskezdes = true;
     }
 }
 
@@ -55,82 +60,116 @@ function chip(mtet) {
 }
 
 function nyeremenyel() {
+    nyeremeny = 0;
+
     fogadasok.forEach(fogadas => {
-
-        if (fogadas.szam == nyeroSzam) {
-            nyeremeny = fogadas.tet * 36;
-        }
-        else{
-            if (fogadas.szam == 40) {
-                if(valasztas["40"].includes(nyeroSzam)){ console.log('Nyertél')}
-                nyeremeny = fogadas.tet * 3;
+        if (fogadas.szam === nyeroSzam) {
+            nyeremeny += fogadas.tet * 36; 
+        } else {
+            if (fogadas.szam === 40) { // 3st
+                if (valasztas["40"].includes(nyeroSzam)) {
+                    nyeremeny += fogadas.tet * 3;
+                }
+            } else if (fogadas.szam === 41) { // 2st
+                if (valasztas["41"].includes(nyeroSzam)) {
+                    nyeremeny += fogadas.tet * 3;
+                }
+            } else if (fogadas.szam === 42) { // 1st
+                if (valasztas["42"].includes(nyeroSzam)) {
+                    nyeremeny += fogadas.tet * 3;
+                }
+            } else if (fogadas.szam === 43) { // 1-12
+                if (valasztas["43"].includes(nyeroSzam)) {
+                    nyeremeny += fogadas.tet * 3;
+                }
+            } else if (fogadas.szam === 44) { // 13-24
+                if (valasztas["44"].includes(nyeroSzam)) {
+                    nyeremeny += fogadas.tet * 3;
+                }
+            } else if (fogadas.szam === 45) { // 25-36
+                if (valasztas["45"].includes(nyeroSzam)) {
+                    nyeremeny += fogadas.tet * 3;
+                }
+            } else if (fogadas.szam === 46) { // 1-18
+                if (valasztas["46"].includes(nyeroSzam)) {
+                    nyeremeny += fogadas.tet * 2;
+                }
+            } else if (fogadas.szam === 47) { // Even
+                if (valasztas["47"].includes(nyeroSzam)) {
+                    nyeremeny += fogadas.tet * 2;
+                }
+            } else if (fogadas.szam === 48) { // Red
+                if (valasztas["48"].includes(nyeroSzam)) {
+                    nyeremeny += fogadas.tet * 2;
+                }
+            } else if (fogadas.szam === 49) { // Black
+                if (valasztas["49"].includes(nyeroSzam)) {
+                    nyeremeny += fogadas.tet * 2;
+                }
+            } else if (fogadas.szam === 50) { // Odd
+                if (valasztas["50"].includes(nyeroSzam)) {
+                    nyeremeny += fogadas.tet * 2;
+                }
+            } else if (fogadas.szam === 51) { // 19-36
+                if (valasztas["51"].includes(nyeroSzam)) {
+                    nyeremeny += fogadas.tet * 2;
+                }
             }
-            else if (fogadas.szam == 41) {
-                if(valasztas["41"].includes(nyeroSzam)){ console.log('Nyertél')}
-                nyeremeny = fogadas.tet * 3;
-            }
-            else if (fogadas.szam == 42) {
-                if(valasztas["42"].includes(nyeroSzam)){ console.log('Nyertél')}
-                nyeremeny = fogadas.tet * 3;
-            }
-            else if (fogadas.szam == 43) {
-                if(valasztas["43"].includes(nyeroSzam)){ console.log('Nyertél')}
-                nyeremeny = fogadas.tet * 3;
-            }
-            else if (fogadas.szam == 44) {
-                if(valasztas["44"].includes(nyeroSzam)){ console.log('Nyertél')}
-                nyeremeny = fogadas.tet * 3;
-            }
-            else if (fogadas.szam == 45) {
-                if(valasztas["45"].includes(nyeroSzam)){ console.log('Nyertél')}
-                nyeremeny = fogadas.tet * 3;
-            }
-            else if (fogadas.szam == 46) {
-                if(valasztas["46"].includes(nyeroSzam)){ console.log('Nyertél')}
-                nyeremeny = fogadas.tet * 2;
-            }
-            else if (fogadas.szam == 47) {
-                if(valasztas["47"].includes(nyeroSzam)){ console.log('Nyertél')}
-                nyeremeny = fogadas.tet * 2;
-            }
-            else if (fogadas.szam == 48) {
-                if(valasztas["48"].includes(nyeroSzam)){ console.log('Nyertél')}
-                nyeremeny = fogadas.tet * 2;
-            }
-            else if (fogadas.szam == 49) {
-                if(valasztas["49"].includes(nyeroSzam)){ console.log('Nyertél')}
-                nyeremeny = fogadas.tet * 2;
-            }
-            else if (fogadas.szam == 50) {
-                if(valasztas["50"].includes(nyeroSzam)){ console.log('Nyertél')}
-                nyeremeny = fogadas.tet * 2;
-            }
-            else if (fogadas.szam == 51) {
-                if(valasztas["51"].includes(nyeroSzam)){ console.log('Nyertél')}
-                nyeremeny = fogadas.tet * 2;
-            }
-        }
-
-        if (nyeremeny > 0) {
-            console.log(`Nyertél ${nyeremeny} egységet a ${fogadas.szam} fogadással!`);
         }
     });
+
+    if (nyeremeny > 0) {
+        console.log(`Nyertél ${nyeremeny} egységet!`);
+    }
 }
 
 
+
 document.addEventListener('DOMContentLoaded', function() {
+    const elmultt = confirm("Elmultál 18 éves?");
+
+    if (!elmultt) {
+        window.location.href = "https://www.youtube.com/watch?v=Q3V0-dxVc-c";
+        return;
+    }
     let kerek = document.getElementById('kerek');
     const szoge = document.querySelector('#kerek');
     let idozito = document.getElementById('ora');
+    let tete = document.getElementById('tete');
     let vissza = 5;
-    szamolas()
+    const ball = document.querySelector('.ball');
+    const radius = 60;
+    let angle = 0;
+
+    let moveDuration = 10000;
+
+    function moveBall() {
+        const kezdes = Date.now();
+        function animacio() {
+            const elapsed = Date.now() - kezdes;
+            if (elapsed < moveDuration) { 
+                const x = Math.cos(angle) * radius;
+                const y = Math.sin(angle) * radius;
+                ball.style.left = `calc(17.5% + ${x}px)`;
+                ball.style.top = `calc(31% + ${y}px)`;
+                angle += 0.05;
+                requestAnimationFrame(animacio);
+            } else {
+                ball.style.animation = 'none';
+            }
+        }
+        animacio();
+    }
     
+    
+    szamolas();
     function szamolas(){
         const countdown = setInterval(() => {
             if (vissza <= 0) {
                 clearInterval(countdown);
                 idozito.textContent = "Idő lejárt!";
-                increaseSpeed();
+                kerekf();
+                moveBall();
                 startGame();
                 nyeremenyel();
             } else {
@@ -145,17 +184,19 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log("Nyerő szám:", nyeroSzam);
     }
 
-    function increaseSpeed() {
+    function kerekf() {
         let szog = Math.floor(Math.random() * (4000 - 3000 + 1)) + 3000;
         szoge.style.setProperty('--spin-degrees', `${szog}deg`);
-        kerek.style.animation = 'spin 4s linear forwards';
+        kerek.style.animation = 'spin-slow 10s cubic-bezier(0.33, 0, 0.67, 1) forwards';
         setTimeout(() => {
             kerek.style.animation = 'none';
+            ball.style.animation = 'none';
             void kerek.offsetWidth;
             vissza = 10;
             fogadasok = [];
+            tete.innerHTML = ``;
+            szamolas();
             alert("A nyerő szám: " + nyeroSzam + ". A nyereményed: " + nyeremeny);
-            szamolas()
-        }, 4000);
-    }
+        }, 10000);
+    }    
 });
