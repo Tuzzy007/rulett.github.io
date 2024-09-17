f230771e5c60740814430a173e77b044134468f4
Tuzzy007 <121279943+Tuzzy007@users.noreply.github.com>
2024-09-17 06:42:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ document.addEventListener('DOMContentLoaded', function() {
         setTimeout(() => {
             kerek.style.animation = 'none';
             void kerek.offsetWidth;
-            vissza = 20;
+            vissza = 10;
             fogadasok = [];
             alert("A nyerő szám: " + nyeroSzam + ". A nyereményed: " + nyeremeny);
             szamolas()
